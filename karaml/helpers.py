6b7e9a71998fe86ac95860797a9dc86da7d2584b
Alessandro Ceserani <aceserani@gmail.com>
2023-04-09 14:20:53 -0500

    docs: Add doc strings to various helper functions

--- karaml/helpers.py
+++ karaml/helpers.py
@@ -19,6 +19,10 @@ def dict_eval(string: str) -> dict:
 
 
 def flag_check(string: str) -> bool:
+    """
+    Returns the value of the flag in the string for the to-event options
+    'lazy' and 'repeat' after checking if the flag is valid.
+    """
     flags = {"+": True, "-": False}
     if (flag := string[0]) in flags:
         return flags[flag]
@@ -26,11 +30,31 @@ def flag_check(string: str) -> bool:
 
 
 def get_multi_keys(string: str) -> list:
+    """
+    Returns a list of keys from a string of keys separated by a '+'.
+    The '+' char is used to concatenate multiple keys in a 'from' or 'to'
+    event, e.g.
+
+    j+k: escape + notify(id1, Hello World)
+
+    The above example will send the escape key when the j and k keys are
+    pressed simultaneously, and will also send a notification with the
+    message 'Hello World'. White space is ignored in the string.
+    """
     if "+" in string:
         return [s.strip() for s in string.split("+")]
 
 
 def is_layer(string: str) -> bool:
+    """
+    Returns True if the string matches the regex for an acceptable layer name.
+
+    This function is used for a different purpose than the validate_layer_name
+    function. It is used to determine what kind of to-event to assign the
+    string to. If it is a layer, we might want to assign it to a 'to' event
+    rather than a 'to_if_held_down' event so the layer can be accessed
+    immediately without waiting for the 'to_if_held_down_threshold'.
+    """
     return search("^/(\\w+)/$", string)
 
 
@@ -43,6 +67,21 @@ def make_list(x) -> list:
 
 
 def translate_params(params: dict) -> dict:
+    """
+    Convert the aliased parameter keys to valid parameter names for the
+    following parameters:
+
+    a -> basic.to_if_alone_timeout_milliseconds
+    h -> basic.to_if_held_down_threshold_milliseconds
+    d -> basic.to_delayed_action_delay_milliseconds
+    s -> basic.simultaneous_threshold_milliseconds
+    m -> mouse_motion_to_scroll.speed
+
+    The default parameter names are used if the parameter is not in the
+    param_aliases dict. These parameters can be overridden by the user
+    in the Karabiner-Elements GUI.
+    """
+
     param_aliases = {
         "a": "basic.to_if_alone_timeout_milliseconds",
         "h": "basic.to_if_held_down_threshold_milliseconds",
@@ -79,6 +118,14 @@ def validate_condition_dict(condition_dict: dict):
 
 
 def validate_shnotify_dict(notification_dict: dict):
+    """
+    Confirm that the user is only using valid keys in the shnotify() dict if
+    they are using a dict rather than positional arguments.
+    shnotify() is a wrapper for the shell command 'osascript -e' that
+    sends a notification to the user via the macOS Notification Center using
+    AppleScript. The dict keys are the same as some AppleScript arguments
+    for the 'display notification' command.
+    """
     valid = ["msg", "title", "subtitle", "sound"]
     for k in notification_dict:
         if k not in valid:
@@ -86,10 +133,25 @@ def validate_shnotify_dict(notification_dict: dict):
 
 
 def validate_layer(string: str) -> str:
+    """
+    Returns the layer name if the string matches the regex for an acceptable
+    layer name. Otherwise, it raises an error.
+
+    The format must be a string enclosed in forward slashes, e.g. '/layer1/'
+    with only alphanumeric characters and underscores.
+    """
     return search("^/(\\w+)/$", string).group(1) or invalidLayerName(string)
 
 
 def validate_mod_aliases(mods: str) -> str:
+    """
+    Check that all the modifier aliases in the string are valid.
+    The return value is only used for testing. In the main program, this
+    function is called for its side effect of raising an error if the
+    string contains an invalid modifier alias. So the original modifier string
+    is used in the main program after this function is called.
+    """
+
     mods = mods.replace("(", "").replace(")", "")
     for char in mods:
         if char not in MODIFIERS.keys():
@@ -98,6 +160,11 @@ def validate_mod_aliases(mods: str) -> str:
 
 
 def validate_to_opts(string: str) -> str:
+    """
+    Check that the optional modifier is valid. This function is only called
+    if the UserMapping object has an 'opts' attribute, which is only set
+    if the user added a to_opts argument in the correct position in a map.
+    """
     valid = ["lazy", "repeat"]
     if (opt := string[1:]) in valid:
         return opt
